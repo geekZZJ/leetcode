0747545925b999465dd48ffcabec41ac1fc34a4e
zzj <2461927976@qq.com>
2020-11-14 12:55:22 +0800

    初始化项目

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+/*
+ * @Author: zzj
+ * @Date: 2020-01-12 16:34:01
+ * @LastEditors: zzj
+ * @LastEditTime: 2020-11-14 12:46:11
+ * @Description:
+ */
+module.exports = {
+  extends: ["standard", "plugin:jest/recommended"],
+};
---
+++ test/sum.test.js
@@ -0,0 +1,12 @@
+/*
+ * @Author: zzj
+ * @Date: 2020-01-12 16:34:01
+ * @LastEditors: zzj
+ * @LastEditTime: 2020-11-14 12:47:20
+ * @Description:
+ */
+import sum from "../code/sum";
+
+test("adds 1 + 2 to equal 3", () => {
+  expect(sum(1, 2)).toBe(3);
+});
