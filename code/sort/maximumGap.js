3a528b46c070028c72f2a0cf41ab896b3ef5db52
zzj <2461927976@qq.com>
2020-11-29 12:17:22 +0800

    性能提升

--- code/sort/maximumGap.js
+++ code/sort/maximumGap.js
@@ -2,23 +2,48 @@
  * @Author: zzj
  * @Date: 2020-11-29 11:36:21
  * @LastEditors: zzj
- * @LastEditTime: 2020-11-29 11:49:57
+ * @LastEditTime: 2020-11-29 12:15:27
  * @Description:(164)最大间距
  */
+// export default (nums) => {
+//   if (nums.length < 2) {
+//     return 0;
+//   }
+//   // sort默认从小到大
+//   nums.sort((a, b) => {
+//     return a - b;
+//   });
+//   let max = 0;
+//   for (let i = 0, tmp; i < nums.length - 1; i++) {
+//     tmp = nums[i + 1] - nums[i];
+//     if (tmp > max) {
+//       max = tmp;
+//     }
+//   }
+//   return max;
+// };
+
 export default (nums) => {
   if (nums.length < 2) {
     return 0;
   }
-  // sort默认从小到大
-  nums.sort((a, b) => {
-    return a - b;
-  });
   let max = 0;
-  for (let i = 0, tmp; i < nums.length - 1; i++) {
-    tmp = nums[i + 1] - nums[i];
-    if (tmp > max) {
-      max = tmp;
+  let len = nums.length - 1;
+  let space;
+  for (let i = len, tmp; i > 0; i--) {
+    for (let j = 0; j < i; j++) {
+      tmp = nums[j];
+      if (tmp > nums[j + 1]) {
+        nums[j] = nums[j + 1];
+        nums[j + 1] = tmp;
+      }
+    }
+    if (i < len) {
+      space = nums[i + 1] - nums[i];
+      if (space > max) {
+        max = space;
+      }
     }
   }
-  return max;
+  return Math.max(max, nums[1] - nums[0]);
 };
--- test/sort/maximumGap.test.js
+++ test/sort/maximumGap.test.js
@@ -2,11 +2,11 @@
  * @Author: zzj
  * @Date: 2020-11-29 11:36:43
  * @LastEditors: zzj
- * @LastEditTime: 2020-11-29 11:48:15
+ * @LastEditTime: 2020-11-29 12:11:10
  * @Description:
  */
 import maximumGap from "../../code/sort/maximumGap";
 
 test("最大间距", () => {
-  expect(maximumGap([1, 3, 100])).toBe(97);
+  expect(maximumGap([13, 16, 19, 1])).toBe(12);
 });
