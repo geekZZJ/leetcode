b64b0a09964fb9e7ceea027278d5d0815191cf72
zzj <2461927976@qq.com>
2021-02-19 22:23:25 +0800

    冒泡排序纠错

--- code/sort/bubbling.js
+++ code/sort/bubbling.js
@@ -2,11 +2,11 @@
  * @Author: zzj
  * @Date: 2020-11-28 17:49:55
  * @LastEditors: zzj
- * @LastEditTime: 2020-11-28 18:03:16
+ * @LastEditTime: 2021-02-19 22:21:19
  * @Description:冒泡排序
  */
 export default (arr) => {
-  for (let i = arr.length, tmp; i > 0; i--) {
+  for (let i = arr.length - 1, tmp; i > 0; i--) {
     for (let j = 0; j < i; j++) {
       tmp = arr[j];
       if (tmp > arr[j + 1]) {
