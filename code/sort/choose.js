e715097753cb73c3018df9cef69b51a8e1ec8e93
zzj <2461927976@qq.com>
2021-02-21 18:26:51 +0800

    选择排序

--- code/sort/choose.js
+++ code/sort/choose.js
@@ -2,23 +2,23 @@
  * @Author: zzj
  * @Date: 2020-11-28 18:13:37
  * @LastEditors: zzj
- * @LastEditTime: 2021-02-21 18:25:22
+ * @LastEditTime: 2021-02-21 18:26:44
  * @Description:选择排序
  */
-// export default (arr) => {
-//   for (let i = 0, min; i < arr.length; i++) {
-//     min = arr[i];
-//     for (let j = i + 1; j < arr.length; j++) {
-//       if (arr[j] < min) {
-//         let c = min;
-//         min = arr[j];
-//         arr[j] = c;
-//       }
-//     }
-//     arr[i] = min;
-//   }
-//   return arr;
-// };
+export default (arr) => {
+  for (let i = 0, min; i < arr.length; i++) {
+    min = arr[i];
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < min) {
+        let c = min;
+        min = arr[j];
+        arr[j] = c;
+      }
+    }
+    arr[i] = min;
+  }
+  return arr;
+};
 
 function selectionSort(arr) {
   for (let i = 0; i < arr.length - 1; i++) {
