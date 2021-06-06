82f50a031969b9eabbd6f824762d96c1d1f24460
zzj <2461927976@qq.com>
2021-06-06 17:39:36 +0800

    中序遍历非递归算法

--- code/btree/inorder.js
+++ code/btree/inorder.js
@@ -2,18 +2,35 @@
  * @Author: zzj
  * @Date: 2021-06-06 16:22:52
  * @LastEditors: zzj
- * @LastEditTime: 2021-06-06 16:27:50
+ * @LastEditTime: 2021-06-06 17:38:31
  * @Description:中序遍历
  */
 const bt = require("./bt");
+
+// const inorder = (root) => {
+//   if (!root) {
+//     return;
+//   }
+//   inorder(root.left);
+//   console.log(root.val);
+//   inorder(root.right);
+// };
 
 const inorder = (root) => {
   if (!root) {
     return;
   }
-  inorder(root.left);
-  console.log(root.val);
-  inorder(root.right);
+  const stack = [];
+  let p = root;
+  while (stack.length || p) {
+    while (p) {
+      stack.push(p);
+      p = p.left;
+    }
+    const n = stack.pop();
+    console.log(n.val);
+    p = n.right;
+  }
 };
 
 inorder(bt);
